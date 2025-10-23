e54699955d81747268dc88672c4ab591d3edcf17
j-timothy1234 <jobingetimothyosubert@gmail.com>
2025-10-23 21:37:15 +0300

    Auto-sync: file changes

--- clientDashboard/clientDashboard.php
+++ clientDashboard/clientDashboard.php
@@ -125,7 +125,7 @@ if ($thumbCandidate && file_exists(__DIR__ . '/../' . $thumbCandidate)) {
                         </a>
                     </li>
                     <li class="nav-item mt-auto">
-                        <a class="nav-link text-danger" href="../loginlogout.php">
+                        <a class="nav-link text-danger" href="../login/login.php">
                             <i class="fas fa-sign-out-alt me-2"></i>LOGOUT
                         </a>
                     </li>
@@ -601,4 +601,4 @@ if ($thumbCandidate && file_exists(__DIR__ . '/../' . $thumbCandidate)) {
 
 </body>
 
-</
+</html>
