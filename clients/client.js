7648ebb53a44dfdbeb7c232b0f5a72b71565c598
j-timothy1234 <jobingetimothyosubert@gmail.com>
2025-10-25 23:24:59 +0300

    Auto-sync: file changes

--- clients/client.js
+++ clients/client.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     const confirmPassword = $("#confirmPassword").val();
 
     // Username: letters, Spaces, Special characters ie Job Tim, J-Tim
-    if (!/^[A-Za-z][A-Za-z\s\-']*[A-Za-z]$/.test(username)) {
+    if (!/^[A-Z][A-Za-z\s\-']*[A-Za-z]$/.test(username)) {
       alert("Username must start and end with a letter, and can contain spaces, hyphens, or apostrophes(e.g., Job Timothy, J-Timothy, O'Connor).");
       return;
     }
