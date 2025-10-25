ddcf0d36f4c15431bf845b6a7c9ccad21de721aa
j-timothy1234 <jobingetimothyosubert@gmail.com>
2025-10-26 01:09:06 +0300

    Auto-sync: file changes

--- Local MariaDB.session.sql
+++ Local MariaDB.session.sql
@@ -3,7 +3,3 @@ use campusLink;
 show tables;
 select * from drivers;
 select * from clients;
-select * from riders;
-select Rider_ID, Username, Phone_Number, Email from riders;
-select Driver_ID, Username, Phone_Number, Email from drivers;
-select Client_ID, Username, Phone_Number, Email from clients;
